afe289156c51491969744e7401d6d5a795d9ea92
Jéssica Mota <marceline0713@gmail.com>
2022-07-17 19:12:51 -0300

    Subindo configuracoes e Exceptions

---
+++ src/main/java/br/com/pi_adocao_pet_back/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.pi_adocao_pet_back.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private JwtProvider jwtProvider;
+
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = 
+				new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+}
---
+++ src/main/java/br/com/pi_adocao_pet_back/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package br.com.pi_adocao_pet_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customizarSwagger() {
+		return new OpenAPI()
+				.info( new Info()
+						.title("RestFul API - Projeto Integrador Adoçãopet")
+						.version("v1")
+						.description("API desenvolvida como forma de projeto integrador DH/Serasa")
+						.termsOfService("http://swagger.io/terms")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+					);
+	}
+}
+
